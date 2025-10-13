2c6d9c91dfca6a47fb4ec41d26624b1fb8d230dd
kanchana <kanchanavedkan@gmail.com>
2025-10-13 20:27:44 +0530

    function

---
+++ Notes/lect-6/Function/funcEx.py
@@ -0,0 +1,13 @@
+def greating(name):
+    print(f"hi, Hello, {name}")
+
+# calling function
+greating("kanachana")
+
+def add(x,y):
+    return x+y
+addition =add(5,6)
+print(addition)
+
+total = add(13,14)
+print(total)
